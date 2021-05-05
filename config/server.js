5e79946dff4f55eaabc5028fdf9b14dae4dec3a5
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2021-05-04 23:41:40 -0400

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00773c55c8db8620043ad281a2dbdb61'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2bd8e423-968c-4d58-aed9-c3bb74c3b64f'
+};
